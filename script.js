6613995d07c9be161447cbb211feca9db188e6d7
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2020-12-13 14:39:27 -0800

    Added setItem

--- script.js
+++ script.js
@@ -43,40 +43,44 @@
 // }
 
 
-createRow();
-createRow();
-createRow();
-createRow();
-createRow();
-createRow();
-createRow();
-createRow();
-createRow();
-createRow();
 
+
+createRow();
 addEventListener();
+const timeOfDay = 0;
 
 function addEventListener(){
 $('.saveBtn').click( function() {
 // console.log('hi');
 const inputNotes$ = $(this).siblings('textarea');
-const key = 'Note:' +0;
+const key = 'Note:' + i;
+console.log(key);
 const value = inputNotes$.val();
-
 localStorage.setItem(key, value);
+
 
 })
 }
+const hoursArr$ = ['9:00 am', '10:00 am', '11:00 am', '12:00 am', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm','5:00 pm', '6:00 pm']
+for( i = 0; i < 12; i++){
+console.log(hoursArr$);
+}
 
 function createRow(){
+    
+for( i = 0; i < 12; i++){
+    
 const div$ = $('<div>').addClass('time-block row');
 const textArea$ = $('<textarea>').addClass('textarea present');
-const button$ = $('<button>').addClass('saveBtn').text('Save Notes');
+const button$ = $('<button>').addClass('saveBtn').text('Save Notes').attr('notesData', i);
+// const hour$ = $('<div>').addClass('hour').text(hoursArr$);
 div$.append(textArea$, button$);
 $('.container').append(div$);
 
-const key = 'Note:' +0;
+const key = 'Note:' + i;
 const localStorageRetrive$ = localStorage.getItem(key);
 textArea$.val(localStorageRetrive$);
 
+
+};
 }
