900dce861222b3f8249eab18d2e761092ff3d1b1
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2020-12-12 20:56:23 -0800

    Added local storage

--- script.js
+++ script.js
@@ -20,33 +20,59 @@
 // const hoursArr$ = ['9:00 am', '10:00 am', '11:00 am', '12:00 am', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm','5:00 pm', '6:00 pm']
 // $.each(hoursArr$, function(index, value){
     
+// const div$ = $('<div>').addClass('time-block row');
+// const textArea$ = $('<textarea>').addClass('textarea');
+// const button$ = $('<button>').addClass('saveBtn').text('Save notes');
+// const hour$ = $('<div>').addClass('hour').text(value);
 
+// console.log(hour$);
+// div$.append(hour$, textArea$, button$);
+// $('.container').append(div$);
+ 
+
+
+
+// // console.log(currentHour);
+
+// // if (currentHour > i){
+// //     textArea$.addClass('present');
+// // }
+
+
+// })
 // }
 
 
-
-
+createRow();
+createRow();
+createRow();
+createRow();
+createRow();
+createRow();
+createRow();
+createRow();
+createRow();
+createRow();
 
 addEventListener();
 
 function addEventListener(){
 $('.saveBtn').click( function() {
 // console.log('hi');
-const value = $(this).siblings('textarea');
-console.log(value.val());
+const inputNotes$ = $(this).siblings('textarea');
+const key = 'Note:' +0;
+const value = inputNotes$.val();
+
+localStorage.setItem(key, value);
+
 })
 }
 
-
-const hoursArr$ = ['9:00 am', '10:00 am', '11:00 am', '12:00 am', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm','5:00 pm', '6:00 pm']
-
-$.each(hoursArr$, function(index, value){
 function createRow(){
 const div$ = $('<div>').addClass('time-block row');
 const textArea$ = $('<textarea>').addClass('textarea present');
 const button$ = $('<button>').addClass('saveBtn').text('Save Notes');
-div$.append(hoursArr$[index], textArea$, button$);
+div$.append(textArea$, button$);
 $('.container').append(div$);
+
 }
-createRow();
-});
