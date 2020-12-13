4404fd30ab332e87473dd211384da48d52cdd2a2
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2020-12-12 19:49:49 -0800

    Added save text function

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
 
-renderTimeRows();
+// renderTimeRows();
 // addSaveNotesEventListener();
 
-// function addSaveNotesEvenListener(){
+// function addEvenListener(){
 //     $('.saveBtn').click(function(){
-//         alert($(this).attr('data-time-slot'));
+//         const key = $(this).attr('data-time-slot');
+//         const value = $(this).siblings('textarea').val()
+//     localStorage.setItem(key, value); 
 
 //     })
 
 // }
-function renderTimeRows(){
+
+// //Creates roows, buttons and time sections
+// function renderTimeRows(){
     
-// const currentHour = moment().hour();
+// // const currentHour = moment().hour();
 
-const hoursArr$ = ['9:00 am', '10:00 am', '11:00 am', '12:00 am', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm','5:00 pm', '6:00 pm']
-$.each(hoursArr$, function(index, value){
+// const hoursArr$ = ['9:00 am', '10:00 am', '11:00 am', '12:00 am', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm','5:00 pm', '6:00 pm']
+// $.each(hoursArr$, function(index, value){
     
-const div$ = $('<div>').addClass('time-block row');
-const textArea$ = $('<textarea>').addClass('textarea');
-const button$ = $('<button>').addClass('saveBtn').text('Save notes');
-const hour$ = $('<div>').addClass('hour').text(value);
 
-console.log(hour$);
-div$.append(hour$, textArea$, button$);
-$('.container').append(div$);
-
-// console.log(currentHour);
-
-// if (currentHour > i){
-//     textArea$.addClass('present');
 // }
-// const button$ = $('<button>').adddClass('saveBtn').text('Save Notes').attr('data-time-slot', i);
 
 
+
+
+
+addEventListener();
+
+function addEventListener(){
+$('.saveBtn').click( function() {
+// console.log('hi');
+const value = $(this).siblings('textarea');
+console.log(value.val());
 })
 }
 
 
+const hoursArr$ = ['9:00 am', '10:00 am', '11:00 am', '12:00 am', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm','5:00 pm', '6:00 pm']
 
+$.each(hoursArr$, function(index, value){
+function createRow(){
+const div$ = $('<div>').addClass('time-block row');
+const textArea$ = $('<textarea>').addClass('textarea present');
+const button$ = $('<button>').addClass('saveBtn').text('Save Notes');
+div$.append(hoursArr$[index], textArea$, button$);
+$('.container').append(div$);
+}
+createRow();
+});
